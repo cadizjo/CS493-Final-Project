c89f5e8bf5574800c578de69db71c28d9407c46f
Sean McCoy <mccoyse@oregonstate.edu>
2024-06-11 12:15:42 -0700

    user model adjustment

--- models/user.js
+++ models/user.js
@@ -5,16 +5,22 @@ const sequelize = require('../lib/sequelize')
 const bcrypt = require('bcryptjs')
 
 const User = sequelize.define('user', {
-  name: { type: DataTypes.STRING, allowNull: false },
-  email: { type: DataTypes.STRING, allowNull: false, unique: true },
-  password: { 
-    type: DataTypes.STRING, 
-    allowNull: false,
-    set(value) {
-        this.setDataValue('password', bcrypt.hashSync(value, 8))
+    name: { type: DataTypes.STRING, allowNull: false },
+    email: { type: DataTypes.STRING, allowNull: false, unique: true },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        set(value) {
+            this.setDataValue('password', bcrypt.hashSync(value, 8))
+        }
+    },
+    role: {
+        type: DataTypes.STRING, 
+        allowNull: false, 
+        validate: {
+            isIn: [['admin', 'instructor', 'student']]
+        }
     }
-  },
-  role: { type: DataTypes.STRING, allowNull: false, isIn: [['admin', 'instructor', 'student']] }
 })
 
 exports.User = User
@@ -24,13 +30,13 @@ exports.User = User
  * on users.
  */
 exports.UserClientFields = [
-  'name',
-  'email',
-  'password',
-  'role'
+    'name',
+    'email',
+    'password',
+    'role'
 ]
 
 exports.validateCredentials = async function (email, password) {
-    const user = await User.findOne({ where: { email: email }})
+    const user = await User.findOne({ where: { email: email } })
     return user && await bcrypt.compare(password, user.password)
 }
