ae4797abe8628520214d4a17f5b4b270cda91ba5
Jonah Cadiz <cadizjo@oregonstate.edu>
2024-06-11 15:48:51 -1000

    create scripts for initdb and redis container

--- scripts/run_containers.sh
+++ scripts/run_containers.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # run this script with bash to start containers that were already created with make_contaiers.sh
 docker start mysql-server-final
+docker start redis-server
 sleep 8
 docker start final-api
